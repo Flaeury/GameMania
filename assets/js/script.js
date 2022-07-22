37ab4ae78d55e84e2461d1e56a94fd15c2953c09
Raiane <raianw200410@outlook.com>
2022-07-22 11:39:03 -0300

    gitbuh2

---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+
+    let slideAtual = 1
+    let listaSlides = ["banner-promocao", "banner-gabinete", "banner-mouse"]
+
+    setInterval(mudarSlide, 2500)
+
+    function mudarSlide() {
+
+        // remover slide anterior
+        if (slideAtual > 0) {
+
+        $("#carrossel").removeClass(listaSlides[slideAtual-1])
+
+        }else{
+
+            $("#carrossel").removeClass(listaSlides[listaSlides.length-1])
+            
+        }
+
+        //exibir slide atual 
+        $("#carrossel").addClass(listaSlides[slideAtual])
+
+        //indicar qual slide atual
+        slideAtual++
+
+        if (slideAtual > 2) {
+            slideAtual = 0
+        }
+
+        if (slideAtual > 0) {
+            $("#carrossel").removeClass("conteudo-banner")
+        }
+    }
+
+
+
+
+
+
+
+
+
+    
+    $("#barras").click(function(){
+       if ($("#menu").hasClass("menu-ativo")) {
+            $("#menu").removeClass("menu-ativo")
+       }else{
+            $("#menu").addClass("menu-ativo")
+       }
+    })
+})
+
+function cadastrarNewsletter() {
+    let email = document.getElementById("campo-email").value 
+    console.log(email)
+}
+
+function mostrarMenu() {
+    //Identificar o elemento MENU
+    let menu = document.getElementById("menu") 
+
+    if(getComputedStyle(menu).display == "none") {
+        menu.style.display = "flex"
+    }else{
+        menu.style.display = "none"
+    }
+
+    //Mudar visualização do menu
+   
+}
